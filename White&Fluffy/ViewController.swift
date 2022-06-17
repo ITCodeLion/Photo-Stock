91ad742d97053abcd27e2e6b6772917d378dbc8f
ITCodeLion <levklr@rambler.ru>
2022-06-17 20:58:26 +0300

    init commit

---
+++ White&Fluffy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  White&Fluffy
+//
+//  Created by Lev on 17.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
